45568f54653754c235207357197b6ef39af024fe
luizpsilva <luiz.paulo.09@hotmail.com>
2019-12-02 00:26:53 -0400

    Adicionando configurações para build de produção e arquivos de deploy do app no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
+const api = 'http://ec2-3-83-15-79.compute-1.amazonaws.com:8080/java-cloud/v1';
+
 export const environment = {
-  production: true
+  production: true,
+  vehicle: {
+    all: `${api}/vehicle/all`,
+    save: `${api}/vehicle/save`,
+    edit: `${api}/vehicle/update`,
+    byId: `${api}/vehicle/`,
+    delete: `${api}/vehicle/delete`
+  },
+  vehicle_type: {
+    all: `${api}/vehicle-type/all`,
+    save: `${api}/vehicle-type/save`,
+    edit: `${api}/vehicle-type/update`,
+    byId: `${api}/vehicle-type/`,
+    delete: `${api}/vehicle-type/delete`
+  }
 };
